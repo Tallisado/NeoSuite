d301923a6fd735c133d2bfb9e6ab3ad68c1b64cd
tallisado <Tallis.Vanek@adtran.com>
2013-11-12 11:28:51 -0500

    cleaned up webrobot API and added comments

--- toolbox/webrobot/lib/helper.rb
+++ toolbox/webrobot/lib/helper.rb
@@ -6,288 +6,291 @@ require 'sauce/rspec'
 
 module WebRobotSingletonMethods
 
-	    def drag_and_drop(draggable_element_type, draggable_element, droppable_element_type, droppable_element)
-    p "-- dragging (#{draggable_element}) and dropping into (#{droppable_element})"
-    draggable = self.find_element(draggable_element_type, draggable_element)
-    droppable = self.find_element(droppable_element_type, droppable_element)
-	
-	self.action.click_and_hold(draggable).perform
-	self.action.drag_and_drop(draggable, droppable).perform
-	self.action.release.perform
-   end
-	 
-	 	def wait_for_element_present( how, what, how_long=5)
+	def drag_and_drop(draggable_element_type, draggable_element, droppable_element_type, droppable_element)
+		p "-- dragging (#{draggable_element}) and dropping into (#{droppable_element})"
+		draggable = self.find_element(draggable_element_type, draggable_element)
+		droppable = self.find_element(droppable_element_type, droppable_element)
+		self.action.click_and_hold(draggable).perform
+		self.action.drag_and_drop(draggable, droppable).perform
+		self.action.release.perform
+	end
+
+	def wait_for_element_present( how, what, how_long=5)
 		p("-- wait_for_element_present [ #{how.to_s}," + what + "," + how_long.to_s + " ]")
 		wait_for_it = Selenium::WebDriver::Wait.new(:timeout => how_long )
 		wait_for_it.until { self.find_element(how, what) }
 	end
+
+	# @name is_element_present
+	# @desc checks that the element is found repeatedly until the timer expires
+	def is_element_present( how, what, how_long=5)
+		p("-- check_for_element_present [ #{how.to_s}," + what + "," + how_long.to_s + " ]")
+		wait_for_it = Selenium::WebDriver::Wait.new(:timeout => how_long )
+		wait_for_it.until { self.find_element(how, what) }
+			return true
+		rescue Selenium::WebDriver::Error::NoSuchElementError, Selenium::WebDriver::Error::TimeOutError => ex
+			return false
+	end
+
+	# @name wait_for_element_not_present
+	# @desc unused
+	def wait_for_element_not_present locator, timeout = 120
+		self.wait_for_condition("!selenium.isElementPresent(\"#{locator}\")", timeout)
+	end
+
+	# @name wait_for_element_visible
+	# @desc unused
+	def wait_for_element_visible locator, timeout = 120
+		p "-- waiting for visible #{locator}"
+		wait_for_element_present locator, timeout
+		self.wait_for_condition("selenium.isVisible(\"#{locator}\")", timeout)
+	end
+
+	# @name wait_for_element_not_visible
+	# @desc unused
+	def wait_for_element_not_visible locator, timeout = 120
+		wait_for_element_present locator, timeout
+		self.wait_for_condition("!selenium.isVisible(\"#{locator}\")", timeout)
+	end
+
+	# @name wait_for_text_present
+	# @desc unused
+	def wait_for_text_present locator, timeout = 120
+		self.wait_for_condition("selenium.isTextPresent(\"#{locator}\")", timeout)
+	end
+
+	# @name get_body_text
+	# @desc get the context text from the body tag of the html object
+	def get_body_text
+		return self.get_body_text()
+	end
+
+	# @name verify_text_from_element
+	# @desc verify that the text is located within the target element
+	def verify_text_from_element(method, location_id, compare_value)
+		p ("locating element [ " + location_id + " / " + method.to_s + " ] for the comparative text: " + compare_value)
+		t = self.find_element(method, location_id).text
+		p("-- current value in a given location_id: " + t)
+		self.find_element(method, location_id).text.should == compare_value
+		#verify { self.find_element(method, location_id).text.should == compare_value }
+	end
+
+	# @name verify_attribute_from_element
+	# @desc verify that the attribute is located within the targer element
+	def verify_attribute_from_element(method, location_id, location_attribute, compare_value)
+		p ("locating element [ " + location_id + " / " + method.to_s + " ] @" + location_attribute + " for the comparative text: " + compare_value)
+		a = self.find_element(method, location_id).attribute(location_attribute)
+		p("-- current attribute value given the location and attribute name : " + a)
+		a.should == compare_value
+		#verify { a == compare_value }
+	end
+
+	# @name get_sessionid_from_cookie
+	# @desc
+	def get_sessionid_from_cookie
+		a=Hash[*self.manage.all_cookies]
+		a.each {|key, value| return value if key == "value".intern}
+	end
+
+	# @name clear_cookies_disabled
+	# @desc unused
+	def clear_cookies_disabled
+		p("-- clearing browser_token cookie...")
+		self.delete_cookie("browser_token", "recurse=true")
+	end
+
+	# @name clear_cookies
+	# @desc clear browser's entire cookie jar
+	def clear_cookies
+		p("-- clearing browser cookie cache...")
+		# js =  "var cookie_date = new Date ( ); cookie_date.setTime ( cookie_date.getTime() - 1 );"
+		# js += " document.cookie = 'browser_token =; expires=' + cookie_date.toGMTString();"
+		# p("-- javascript: " + js + "\n\n")
+		# self.run_script(js)
+		self.manage.delete_all_cookies
+	end
+
+	# @name force_session_expire
+	# @desc clearing the cookies forces the session to be reset and will expire immediately
+	def force_session_expire
+		p("-- forcing browser session expire...");
+		clear_cookies
+	end
+
+	# @name sleep_in_slow_mode_a
+	# @desc sleep time configurable in the yaml
+	def sleep_in_slow_mode time=1
+		sleep time if @CONFIG["slow_mode"]
+	end
 	
-   # @name read_yaml_file
-   # @desc 
-   def is_element_present( how, what, how_long=5)
-      p("-- check_for_element_present [ #{how.to_s}," + what + "," + how_long.to_s + " ]")
-	  wait_for_it = Selenium::WebDriver::Wait.new(:timeout => how_long )
-	  wait_for_it.until { self.find_element(how, what) }
-   	     return true
-	  rescue Selenium::WebDriver::Error::NoSuchElementError, Selenium::WebDriver::Error::TimeOutError => ex
-	     return false
-   end
+	# @name wait_for_page_and_verify_text
+	# @desc wait for page to load and verify text
+	def wait_for_page_and_verify_text(text)
+		self.wait_for_page_to_load(120000)
+		check_page_and_verify_text(text)
+	end
 
-   # @name read_yaml_file
-   # @desc 
-   def wait_for_element_not_present locator, timeout = 120
-      self.wait_for_condition("!selenium.isElementPresent(\"#{locator}\")", timeout)
-   end
-
-   # @name read_yaml_file
-   # @desc 
-   def wait_for_element_visible locator, timeout = 120
-      p "-- waiting for visible #{locator}"
-      wait_for_element_present locator, timeout
-      self.wait_for_condition("selenium.isVisible(\"#{locator}\")", timeout)
-   end
-
-   # @name read_yaml_file
-   # @desc 
-   def wait_for_element_not_visible locator, timeout = 120
-      wait_for_element_present locator, timeout
-      self.wait_for_condition("!selenium.isVisible(\"#{locator}\")", timeout)
-   end
-
-   # @name read_yaml_file
-   # @desc 
-   def wait_for_text_present locator, timeout = 120
-      self.wait_for_condition("selenium.isTextPresent(\"#{locator}\")", timeout)
-   end
-
-   # @name read_yaml_file
-   # @desc 
-   def get_body_text
-      return self.get_body_text()
-   end
-	 
-	    # @name read_yaml_file
-   # @desc 
-   def verify_text_from_element(method, location_id, compare_value)
-      p ("locating element [ " + location_id + " / " + method.to_s + " ] for the comparative text: " + compare_value)
-	  t = self.find_element(method, location_id).text
-	  p("-- current value in a given location_id: " + t)
-	  verify { self.find_element(method, location_id).text.should == compare_value }
-	  #verify { t.to_s == compare_value.to_s }
-   end   
-   
-   # @name read_yaml_file
-   # @desc 
-   def verify_attribute_from_element(method, location_id, location_attribute, compare_value)
-      p ("locating element [ " + location_id + " / " + method.to_s + " ] @" + location_attribute + " for the comparative text: " + compare_value)
-	  a = self.find_element(method, location_id).attribute(location_attribute)
-	  p("-- current attribute value given the location and attribute name : " + a)
-	  verify { a == compare_value }
-   end
-	 
-	   # @name read_yaml_file
-   # @desc 
-   def get_sessionid_from_cookie
-      a=Hash[*self.manage.all_cookies]
-      a.each {|key, value| return value if key == "value".intern}
-   end
-   
-   # @name read_yaml_file
-   # @desc 
-   # -- clear all necessary cookies
-   def clear_cookies_disabled
-      p("-- clearing browser_token cookie...")
-      self.delete_cookie("browser_token", "recurse=true")
-   end
-
-   # @name read_yaml_file
-   # @desc 
-   # -- clear cookies in a creative way
-   def clear_cookies
-      p("-- clearing browser cookie cache...")
-      # js =  "var cookie_date = new Date ( ); cookie_date.setTime ( cookie_date.getTime() - 1 );"
-      # js += " document.cookie = 'browser_token =; expires=' + cookie_date.toGMTString();"
-      # p("-- javascript: " + js + "\n\n")
-      # self.run_script(js)
-	  self.manage.delete_all_cookies
-   end
-   
-   # @name read_yaml_file
-   # @desc 
-   def force_session_expire
-      p("-- forcing browser session expire...");
-      clear_cookies
-   end
-
-   # @name read_yaml_file
-   # @desc 
-   def sleep_in_slow_mode time=1
-     sleep time if @CONFIG["slow_mode"]
-   end
-
-   # -- wait for page to load and verify text
-   # @name read_yaml_file
-   # @desc 
-   def wait_for_page_and_verify_text(text)
-      self.wait_for_page_to_load(120000)
-      check_page_and_verify_text(text)
-   end
-
-   # -- check the page for any abnormalities and verify text
-   # @name read_yaml_file
-   # @desc 
-   def check_page_and_verify_text(text)
-      #check_page(self.find_element(:id, "body").text)
-      verify_text(text)
-   end
-   
-   def check(method, element)
-	  if (element.include? "inputEl") then
-		puts "-- sencha element found"
-		if !self.find_element(method, element[0..-9]).attribute("class").include? "x-form-cb-checked"
-			do_click = true
+	# @name check_page_and_verify_text
+	# @desc check the page for any abnormalities and verify text
+	def check_page_and_verify_text(text)
+			#check_page(self.find_element(:id, "body").text)
+		verify_text(text)
+	end
+	
+	# @name check
+	# @desc combobox will be checked if unchecked (sencha logic looks at class to see if DOM is marked as 'checked)
+	def check(method, element)
+		if (element.include? "inputEl") then
+			puts "-- sencha element found"
+			if !self.find_element(method, element[0..-9]).attribute("class").include? "x-form-cb-checked"
+				do_click = true
+			end
+		else
+			puts "-- non-sencha element found"
+			if !self.find_element(method, element).selected?
+				do_click = true
+			end
 		end
-	  else
-	    puts "-- non-sencha element found"
-		if !self.find_element(method, element).selected?
-		   do_click = true
+
+		if (do_click)
+			self.find_element(method, element).click
+			puts "-- checking element"
 		end
-	  end
-	  
-	  if (do_click) 
-	    self.find_element(method, element).click
-		puts "-- checking element"
-      end  
-   end
-   
-   def uncheck(method, element)
-	  if (element.include? "inputEl") then
+	end
+
+	# @name uncheck
+	# @desc combobox will be unchecked if checked (sencha logic looks at class to see if DOM is marked as 'checked)
+	def uncheck(method, element)
+		if (element.include? "inputEl") then
 		puts "-- sencha element found"
 		if self.find_element(method, element[0..-9]).attribute("class").include? "x-form-cb-checked"
 			do_click = true
 		end
-	  else
-	    puts "-- non-sencha element found"
+		else
+			puts "-- non-sencha element found"
 		if self.find_element(method, element).selected?
-		   do_click = true
+			do_click = true
 		end
-	  end
-	  
-	  if (do_click) 
-	    self.find_element(method, element).click
-		puts "-- unchecking element"
-      end  
-   end
+		end
 
-   # -- click on something
-   # @name read_yaml_file
-   # @desc 
-   def click(method, element)
-      # -- 'element' could be a javascript, then we need to call 'run_script' instead of 'click'
-      if /jQuery/.match(element)
-         p("-- running script: " + element)
-         self.run_script(element)
-      else
-		p("-- clicking on element: " + element.to_s + " by " + method.to_s)
+		if (do_click)
+			self.find_element(method, element).click
+			puts "-- unchecking element"
+		end
+	end
+
+	# @name click
+	# @desc sends a click event to the targeted element. (if jQuery, we execute the js to click that element)
+	def click(method, element)
+		# -- 'element' could be a javascript, then we need to call 'run_script' instead of 'click'
+		if /jQuery/.match(element)
+			p("-- running script: " + element)
+			self.run_script(element)
+		else
+			p("-- clicking on element: " + element.to_s + " by " + method.to_s)
+			self.find_element(method, element).click
+		end
+	end
+	
+	# @name double_click
+	# @desc sends an action sequence to the target element to perform a double click
+	def double_click(method, element)
+		clickable = self.find_element(method, element)
+		p "-- double clicking (#{clickable})"
+		self.action.move_to(clickable).perform
+		self.action.move_to(clickable).double_click.perform
+	end
+
+	# @name click_text_from_combobox
+	# @desc clicks the text from the combobox specified. (built in timer that will wait for the combobox dropdown to be populated before click is issued) 
+	def click_text_from_combobox(method, element, contains_text_to_select)
+		p "-- waiting for element"
+		wait_for_element_present(method, element, 10)
+		p "-- clicking element"
 		self.find_element(method, element).click
-      end
-   end
-   
-   def double_click(method, element)
-	 clickable = self.find_element(method, element)
-	 p "-- double clicking (#{clickable})"	 
-	 self.action.move_to(clickable).perform
-	 self.action.move_to(clickable).double_click.perform
-   end 
-   
-   def click_text_from_combobox(method, element, contains_text_to_select)
-     p "-- waiting for element"
-	 wait_for_element_present(method, element, 10)
-	 p "-- clicking element"
-     self.find_element(method, element).click	 
-	 sleep 1
-	 
- 	 i = 0
-	 elements = self.find_elements(:xpath, "//li[contains(@class, 'x-boundlist-item') and contains(text(),'"+contains_text_to_select+"')]")
-	 while i < elements.length do
-		if elements[i].displayed?
-			elements[i].click			
+		sleep 1
+
+		i = 0
+		elements = self.find_elements(:xpath, "//li[contains(@class, 'x-boundlist-item') and contains(text(),'"+contains_text_to_select+"')]")
+		while i < elements.length do
+			if elements[i].displayed?
+				elements[i].click
 			# rescue Selenium::WebDriver::Error::StaleElementReferenceError =>
 				# sleep 2
 				# elements[i].click
 			# end
-		else
-			i = i+1
+			else
+				i = i+1
+			end
 		end
-	 end
-   end
-	
-   # -- click on something that needs a mouse over
-   # @name read_yaml_file
-   # @desc 
-   def mouseover_and_click(method, element_over, element_click)
+	end
+
+	# @name mouseover_and_click
+	# @desc sends an action to the elements to be clicked after it receives a mouseover event (needed for mouseover javascript validation)
+	def mouseover_and_click(method, element_over, element_click)
 		p("-- clicking on element: " + element_click.to_s + " by " + method.to_s)
 		self.action.move_to(self.find_element(method, element_over)).perform
 		sleep 5
 		self.find_element(method, element_click).click
-		end   
-   
-   # -- click on something that needs a mouse over
-   # @name read_yaml_file
-   # @desc 
-   def click_by_script(element_click)
-	 p("-- execute_script clicking on element: " + element_click.to_s)
-	 self.execute_script("document.getElementById('#{element_click}').click();");
-   end
+	end
 
-   # @name read_yaml_file
-   # @desc 
-   def type(method, element, text)
+	# @name click_by_script
+	# @desc issues a javascript click() event to the element
+	def click_by_script(element_click)
+		p("-- execute_script clicking on element: " + element_click.to_s)
+		self.execute_script("document.getElementById('#{element_click}').click();");
+	end
+
+	# @name type
+	# @desc sends text to the target element (built in sleep to allow text to populate box)
+	def type(method, element, text)
 		p("-- typing text ['#{text}'] into element ['#{element}'] by ['#{method}']")
 		self.find_element(method, element).send_keys text
 		sleep 0.2
-   end
-   
-   # @name read_yaml_file
-   # @desc 
-   def clear(method, element)
-      p("-- clearing text from element ['#{element}'] by ['#{method}']")
-	  self.find_element(method, element).clear
-   end
-	
-   # -- select something
-   # @name read_yaml_file
-   # @desc 
-   def select(element, option)
-      p("-- selecting option ['#{option}'] from select element ['#{element}']")
-      self.select(element, option)
-   end
+	end
 
-   # -- select something
-   # @name read_yaml_file
-   # @desc 
-   def select_by(method, element, how, what)
-      p("-- selecting option ['#{how}'] by ['#{what}'] from element ['#{element}'] by ['#{method}']")
-      #self.find_element(method, element).select_by(how, what)
-	  Selenium::WebDriver::Support::Select.new(self.find_element(:id => element)).select_by :text, what
-      #puts self.find_element(method, element).options
-   end
-   
-   def switch_frame(frame_id)
-	  p("-- switching to frame ['#{frame_id}']")
-	  self.switch_to.frame(frame_id)
-   end
-   
-   def switch_frame_default()
-	  p("-- switching to default frame")
-	  self.switch_to.default_content
-   end
+	# @name clear
+	# @desc removes all the text from the target element
+	def clear(method, element)
+		p("-- clearing text from element ['#{element}'] by ['#{method}']")
+		self.find_element(method, element).clear
+	end
+
+	# @name select
+	# @desc selects the elements with the given option from a 'select' dom object
+	def select(element, option)
+		p("-- selecting option ['#{option}'] from select element ['#{element}']")
+		self.select(element, option)
+	end
+
+	# @name select_by
+	# @desc finds the element of the select item, and issues a select_by :text command to find the text in the available options
+	def select_by(method, element, how, what)
+		p("-- selecting option ['#{what}'] from element ['#{element}'] by ['#{method}']")
+		#self.find_element(method, element).select_by(how, what)
+		Selenium::WebDriver::Support::Select.new(self.find_element(:id => element)).select_by :text, what
+		#puts self.find_element(method, element).options
+	end
+
+	# @name switch_frame
+	# @desc allows frame switching, from default to the one provided
+	def switch_frame(frame_id)
+		p("-- switching to frame ['#{frame_id}']")
+		self.switch_to.frame(frame_id)
+	end
+	
+	# @name switch_frame
+	# @desc
+	def switch_frame_default()
+		p("-- switching to default frame")
+		self.switch_to.default_content
+	end
 end
 
 
 RSpec.configure do |config|
 	config.include HelperMethods
-	if ! ENV['WR_INTERFACE'].nil? 
+	if ! ENV['WR_INTERFACE'].nil?
 		if ENV['WR_INTERFACE'].match('sauce')
 			config.filter_run_including :sauce => true
 		elsif ENV['WR_INTERFACE'].match('local')
@@ -330,11 +333,11 @@ end
 #export SAUCE_ACCESS_KEY=6c3ed64b-e065-4df4-b921-75336e2cb9cf
 
 Sauce.config do |c|
-  if !ENV['WR_NOTUNNEL'].nil? || ENV['WR_INTERFACE'] == 'local' 
+  if !ENV['WR_NOTUNNEL'].nil? || ENV['WR_INTERFACE'] == 'local'
 		puts "[webrobot] helper.rb - NO TUNNEL"
 		c[:start_tunnel] = false
 	end
-	if ! ENV['WR_FFONLY'].nil?  
+	if ! ENV['WR_FFONLY'].nil?
 		c[:browsers] = [
 			["Linux", "Firefox", "19"]
 		]
--- toolbox/webrobot/spec/rake_test.rb
+++ toolbox/webrobot/spec/rake_test.rb
@@ -2,8 +2,8 @@ require 'rake'
 #$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__))
 import 'spec/webrobot.rake'
 
-describe "rakefile from webtester should be found" do
-	it "aa" do		
+describe "import webrobot.rake" do
+	it "should init and load the rakefile" do		
 		Rake.application.init
 		Rake.application.load_rakefile
 		Rake.application['test:sauce'].invoke()
