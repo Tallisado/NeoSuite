d89414fb774171bbfaa7e9d4d005077d000e6e2b
tallisado <Tallis.Vanek@adtran.com>
2013-11-25 17:49:29 -0500

    Fix up remove user test case

--- home/tasks/localadmin_removeuser_webrobot.rb
+++ home/tasks/localadmin_removeuser_webrobot.rb
@@ -5,18 +5,18 @@ describe "Local Admin - Remove User", :local => true do
 		@selenium.login('admin', 'password', 'helpButton-btnIconEl')
 		expect { @selenium.title.should eql("ADTRAN Neo") }.to_not raise_error
 		sleep 3
-		@suite.click(:xpath, '//*[@id="removeUser_wiz"]')
-		@suite.click_text_from_combobox(:id, 'removeUserWiz_comboBox-inputEl', "Automated User")	
+		@selenium.click(:xpath, '//*[@id="removeUser_wiz"]')
+		@selenium.click_text_from_combobox(:id, 'removeUserWiz_comboBox-inputEl', "Automated User")	
 		sleep 5
-		@suite.click(:xpath, '//*[@id="removeUserWizard_next_btn"]')
-		@suite.click(:xpath, '//*[@id="removeUserWizard_save_btn"]')
+		@selenium.click(:xpath, '//*[@id="removeUserWizard_next_btn"]')
+		@selenium.click(:xpath, '//*[@id="removeUserWizard_save_btn"]')
 		sleep 5
 
-		@suite.click(:xpath, '//*[@id="removeUser_wiz"]')
-		@suite.click_text_from_combobox(:id, 'removeUserWiz_comboBox-inputEl', "Reassign Test")	
+		@selenium.click(:xpath, '//*[@id="removeUser_wiz"]')
+		@selenium.click_text_from_combobox(:id, 'removeUserWiz_comboBox-inputEl', "Reassign Test")	
 		sleep 5
-		@suite.click(:xpath, '//*[@id="removeUserWizard_next_btn"]')
-		@suite.click(:xpath, '//*[@id="removeUserWizard_save_btn"]')
+		@selenium.click(:xpath, '//*[@id="removeUserWizard_next_btn"]')
+		@selenium.click(:xpath, '//*[@id="removeUserWizard_save_btn"]')
 		sleep 5
     end
 end
