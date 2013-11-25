5700a91a8f1f015cb2f872dc4237f618710be808
tallisado <Tallis.Vanek@adtran.com>
2013-11-25 14:05:23 -0500

    Add incremental task

--- home/tasks/uiaccessibility_webrobot.rb
+++ home/tasks/uiaccessibility_webrobot.rb
@@ -2,24 +2,16 @@ require File.join(File.dirname(__FILE__), "../../toolbox/webrobot/lib/webrobot_c
 
 describe "NEO Accessibility", :local => true do
 	it "should serve the login page to access assets" do
-		@selenium.navigate.to "http://10.10.9.129/Login/index.php"
-		sleep 1
+		@selenium.navigate.to ENV['BASEURL']
 		expect { @selenium.wait_for_element_present(:id, "loginnameid-inputEl", 15) }.to_not raise_error	
 		@selenium.title.should eql("ADTRAN Neo")
 	end
 	
 	it "should allow us to login", :local => true do
-		@selenium.navigate.to "http://10.10.9.129/Login/index.php"
-		expect {  	
-			@selenium.wait_for_element_present(:id, "loginnameid-inputEl", 15)
-			@selenium.type(:id, "loginnameid-inputEl", 'admin')
-			@selenium.type(:id, "loginpasswordid-inputEl", 'password')
-			sleep 1
-			@selenium.click(:id, "loginbuttonid-btnIconEl")
-			
-			@selenium.relaxed_wait_for_element_present(:id, "createUser_wiz", 5)
-		}.to_not raise_error	
-		@selenium.current_url.should eq("http://10.10.9.129/LocalAdmin/index.php")
+		@selenium.navigate.to ENV['BASEURL']
+		@selenium.login('admin', 'password', 'createUser_wiz')
+		expect { @selenium.title.should eql("ADTRAN Neo") }.to_not raise_error
+		#@selenium.current_url.should eq("http://10.10.9.129/LocalAdmin/index.php")
 		#@selenium.current_url.should eq("http://10.10.9.129/LocalAdmin/index.phpasd")
 	end
 end
