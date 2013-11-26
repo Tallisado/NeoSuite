59a7788a1ddfefa78995b10b75a4f55b4aa66bb2
tallisado <Tallis.Vanek@adtran.com>
2013-11-26 14:37:34 -0500

    Add profiles and tasks for factory resetting units

---
+++ home/tasks/factorysshconnect_rubytask_incremental.rb
@@ -0,0 +1,11 @@
+require_relative "../../toolbox/ssh_commander/lib_commander"
+
+sc = ShellCommander.new(:host => "10.10.9.165", :user => "admin", :password => "password")
+
+puts "-- Starting configuration reset --"
+#Enable
+sc.exec("en")
+#Enter password
+sc.exec("password")
+#show version
+sc.exec("show version")
