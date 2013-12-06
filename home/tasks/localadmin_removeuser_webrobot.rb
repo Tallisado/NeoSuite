82a2f901fb52ce0f2f8af9a01201abeee7cd2641
mikelerner <michael.lerner@adtran.com>
2013-12-06 15:49:30 -0500

    Modify remove user with new ID and hope it works

--- home/tasks/localadmin_removeuser_webrobot.rb
+++ home/tasks/localadmin_removeuser_webrobot.rb
@@ -12,14 +12,14 @@ describe "Local Admin - Remove User", :local => true do
 		expect { @selenium.title.should eql("ADTRAN Neo") }.to_not raise_error
 		sleep 3
 		@selenium.click(:xpath, '//*[@id="removeUser_wiz"]')
-		@selenium.click_text_from_combobox(:id, 'removeUserWiz_comboBox-inputEl', "Automated User")	
+		@selenium.click_text_from_combobox(:name, 'removeUserWizard_window_removeUser_wizard_user_combobox-inputEl', "Automated User")	
 		sleep 5
 		@selenium.click(:xpath, '//*[@id="removeUserWizard_next_btn"]')
 		@selenium.click(:xpath, '//*[@id="removeUserWizard_save_btn"]')
 		sleep 5
 
 		@selenium.click(:xpath, '//*[@id="removeUser_wiz"]')
-		@selenium.click_text_from_combobox(:id, 'removeUserWiz_comboBox-inputEl', "Reassign Test")	
+		@selenium.click_text_from_combobox(:name, 'removeUserWizard_window_removeUser_wizard_user_combobox-inputEl', "Reassign Test")	
 		sleep 5
 		@selenium.click(:xpath, '//*[@id="removeUserWizard_next_btn"]')
 		@selenium.click(:xpath, '//*[@id="removeUserWizard_save_btn"]')
